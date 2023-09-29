9b933dce4258acae64bc44d430e63a5301e4b5b6
timoteostifft <timoteostifft@gmail.com>
2023-09-29 13:52:41 -0300

    style: add response to register controller

--- src/infra/http/controllers/register.ts
+++ src/infra/http/controllers/register.ts
@@ -27,7 +27,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       cpf,
     });
 
-    return reply.status(201);
+    return reply.status(201).send();
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: err.message });
