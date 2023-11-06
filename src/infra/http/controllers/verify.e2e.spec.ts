361e43f4d132a7fe85c5ffe32cd58d3a319557e0
timoteostifft <timoteostifft@gmail.com>
2023-11-06 20:25:52 -0300

    refactor: remove prisma repository injection

--- src/infra/http/controllers/verify.e2e.spec.ts
+++ src/infra/http/controllers/verify.e2e.spec.ts
@@ -1,17 +1,15 @@
-import { Cpf } from "@/domain/entities/value-objects/cpf";
 import { app } from "@/infra/app";
 import { JwtEncrypter } from "@/infra/cryptography/jwt-encrypter";
-import { PrismaPeopleRepository } from "@/infra/database/repositories/prisma-people-repository";
+import { prisma } from "@/infra/database/prisma-service";
+import { hash } from "bcryptjs";
 import * as JwtService from "jsonwebtoken";
 
-let prismaPeopleRepository: PrismaPeopleRepository;
 let jwtEncrypter: JwtEncrypter;
 
 describe("[GET] /verify", () => {
   beforeAll(async () => {
     await app.ready();
 
-    prismaPeopleRepository = new PrismaPeopleRepository();
     jwtEncrypter = new JwtEncrypter(JwtService);
   });
 
@@ -20,36 +18,26 @@ describe("[GET] /verify", () => {
   });
 
   test("[GET] /verify", async () => {
-    await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: {
-        email: "johndoe@example.com",
-        password: "12345678",
-        first_name: "John",
-        last_name: "Doe",
-        cpf: "523.065.281-01",
+    await prisma.account.create({
+      data: {
+        id: "1",
+        email: "account@example.com",
+        password: await hash("12345678", 8),
       },
     });
 
-    const account = await prismaPeopleRepository.findByCpf(
-      Cpf.createFromText("523.065.281-01")
-    );
+    const code = await jwtEncrypter.encrypt({
+      account_id: "1",
+    });
 
-    if (account) {
-      const code = await jwtEncrypter.encrypt({
-        account_id: account.account_id.toString(),
-      });
+    const reply = await app.inject({
+      method: "GET",
+      url: "/verify",
+      query: {
+        code,
+      },
+    });
 
-      const reply = await app.inject({
-        method: "GET",
-        url: "/verify",
-        query: {
-          code,
-        },
-      });
-
-      expect(reply.statusCode).toBe(200);
-    }
+    expect(reply.statusCode).toBe(200);
   });
 });
