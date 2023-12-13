4313e303c68ae66b7a2a741bd30556aceef04083
timoteostifft <timoteostifft@gmail.com>
2023-12-13 18:14:25 -0300

    style: remove unused imports

--- src/infra/http/controllers/search-offers.ts
+++ src/infra/http/controllers/search-offers.ts
@@ -1,12 +1,7 @@
-import { load } from "@tensorflow-models/universal-sentence-encoder";
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
 import { SearchOffersUseCase } from "@/domain/use-cases/search-offers";
-import { TfUseModel } from "@/infra/search/tf-use-model";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
-import { QdrantProductsCollection } from "@/infra/database/collections/qdrant-products-collection";
-import { PrismaProductsRepository } from "@/infra/database/repositories/prisma-products-repository";
-import { PrismaOffersProductsRepository } from "@/infra/database/repositories/prisma-offers-products.repository";
 import { OffersPresenter } from "../presenters/offers-presenter";
 
 const searchOffersQuerySchema = z.object({
