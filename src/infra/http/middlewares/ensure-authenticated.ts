ef3f984d618bf302d19a447b7b60bc4c9d1bcb54
timoteostifft <timoteostifft@gmail.com>
2024-02-29 17:51:35 -0300

    feat: add new access token return via cookies when previous is expired but a session is still valid

--- src/infra/http/middlewares/ensure-authenticated.ts
+++ src/infra/http/middlewares/ensure-authenticated.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/infra/database/prisma-service";
 import { env } from "@/infra/env";
 import { FastifyReply, FastifyRequest } from "fastify";
 import * as JwtService from "jsonwebtoken";
@@ -20,9 +21,43 @@ export async function ensureAuthenticated(
 
     const [, token] = authHeader.split(" ");
 
-    const payload = JwtService.verify(token, env.JWT_SECRET);
+    let payload;
+
+    payload = JwtService.verify(token, env.JWT_SECRET, {
+      ignoreExpiration: true,
+    });
 
     const { sub } = jwtPayloadSchema.parse(payload);
+
+    try {
+      payload = JwtService.verify(token, env.JWT_SECRET);
+    } catch (err) {
+      if (err instanceof JwtService.TokenExpiredError) {
+        const sessionExpiration = new Date(
+          Date.now() - env.SESSION_DURATION_IN_DAYS * 24 * 60 * 60 * 1000
+        );
+
+        const session = await prisma.session.findFirst({
+          where: {
+            account_id: sub,
+            user_agent: request.headers["user-agent"] ?? "not-identified",
+            created_at: {
+              gte: sessionExpiration,
+            },
+          },
+        });
+
+        if (!session) {
+          return reply.status(401).send({ message: "Session expired." });
+        }
+
+        const newAccessToken = JwtService.sign({ sub }, env.JWT_SECRET, {
+          expiresIn: "24h",
+        });
+
+        reply.header("set-cookie", { access_token: newAccessToken });
+      }
+    }
 
     request.payload = {
       sub,
