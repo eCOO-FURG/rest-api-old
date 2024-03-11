2c5425a9c1d2f19c3a0378c451c794a445a23622
timoteostifft <timoteostifft@gmail.com>
2024-03-11 00:00:53 -0300

    refactor: remove schedule

--- src/domain/repositories/schedules-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Schedule } from "../entities/schedule";
-
-export interface SchedulesRepository {
-  save(schedule: Schedule): Promise<void>;
-  findActive(): Promise<Schedule | null>;
-  check(start: Date, end: Date): Promise<Schedule | null>;
-}
