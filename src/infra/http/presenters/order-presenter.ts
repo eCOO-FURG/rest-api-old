8fd732a5bc346f3fed89a7c543c271de305326d9
timoteostifft <timoteostifft@gmail.com>
2024-03-13 12:20:05 -0300

    feat: add id property to order presenter

--- src/infra/http/presenters/order-presenter.ts
+++ src/infra/http/presenters/order-presenter.ts
@@ -3,6 +3,7 @@ import { Order } from "@/domain/entities/order";
 export class OrderPresenter {
   static toHttp(order: Order) {
     return {
+      id: order.id.value,
       payment_method: order.payment_method,
       shipping_address:
         order.payment_method === "ON_DELIVERY" ? null : order.shipping_address,
