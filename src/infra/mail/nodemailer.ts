5edf2ceb7728b477b3f178e48b8275b50667ef48
timoteostifft <timoteostifft@gmail.com>
2024-03-22 00:50:34 -0300

    feat: add nodemailer error handling

--- src/infra/mail/nodemailer.ts
+++ src/infra/mail/nodemailer.ts
@@ -7,11 +7,15 @@ export class Nodemailer implements Mailer {
   constructor(private transporter: Transporter) {}
 
   async send(email: Email): Promise<void> {
-    await this.transporter.sendMail({
-      from: env.ECOO_EMAIL,
-      to: email.to,
-      subject: email.subject,
-      html: email.html,
-    });
+    try {
+      await this.transporter.sendMail({
+        from: env.ECOO_EMAIL,
+        to: email.to,
+        subject: email.subject,
+        html: email.html,
+      });
+    } catch (error) {
+      console.log("Erro ao enviar email: ", error);
+    }
   }
 }
