0000a7a6cc59484c9734fab16d1629a9250fd218
timoteostifft <timoteostifft@gmail.com>
2024-05-19 18:44:10 -0300

    feat: sentry config

---
+++ src/infra/log/sentry.ts
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/node";
+
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import { env } from "../env";
+
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
